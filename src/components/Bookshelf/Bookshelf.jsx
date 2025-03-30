17e39b4c60448bbdcda657ec8a896b6be974ad06
rmcelroy1990 <mcelroy.raeme@gmail.com>
2025-03-30 02:42:09 -0500

    created form

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -30,11 +30,39 @@ const handleSubmut = (event) => {
 <div className="bookshelfDiv">
   <div className="formDiv">
     <h3>Add a Book</h3>
-    {/* Form will go here */}
+    <form onSubmit={handleSubmit}></form>
   </div>
-  <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+        <label htmlFor="title">Title: </label>
+        <input 
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Book Title"
+            value={newBook.title}
+            onChange={handleInputChange}
+        />
+ <div>
+    <label htmlFor="author">Author: </label>
+    <input
+        type="text"
+        id="author"
+        name="author"
+        placeholder="Author"
+        value={newBook.author}
+        onChange={handleInputChange}
+        />
+        </div>
+    <button type="submit">Add Book</button>
+    </form>
+ </div>
+
+  <div className="bookCardsDiv">{/* Book cards will display here */}
+        {books.map((book, index) => (
+    <div key={index} className="book-card">
+    <h3 className="book-title">{book.title}</h3>
+    <p className="book-author">by {book.author}</p>
 </div>
-);
-};
+))}
+</div>
 
 export default Bookshelf;
